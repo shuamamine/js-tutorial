385666289cbd9c2752c9c24c4ea9605da57d94f1
shuamamine <sahasenjuti796@gmail.com>
2024-10-13 18:33:38 +0530

    variables

--- script.js
+++ script.js
@@ -2,3 +2,22 @@ alert('Hello world!');
 
 console.log(1);
 console.log(2);
+
+// variables 
+
+let age = 25;
+let year = 2019;
+
+console.log(age, year);
+
+age = 30;
+console.log(age); // 30 is printed in the console
+
+// const 
+const points = 100;
+points = 50;
+console.log(points);
+
+// var is old way of creating variables
+var score = 75;
+console.log(score);
